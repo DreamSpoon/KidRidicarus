c350cb5fa480ab158574eb7fc1a42bc8b80dff90
DreamSpoon <flaskofwater@gmail.com>
2019-03-25 20:12:04 -0400

    Add room_hbound property to prevent screen moving too far horizontally for Metroid level 1-1.

--- core/src/kidridicarus/common/agent/roombox/RoomBox.java
+++ core/src/kidridicarus/common/agent/roombox/RoomBox.java
@@ -7,6 +7,7 @@ import kidridicarus.agency.Agency;
 import kidridicarus.agency.agent.Agent;
 import kidridicarus.agency.agent.DisposableAgent;
 import kidridicarus.agency.tool.ObjectProperties;
+import kidridicarus.common.info.CommonInfo;
 import kidridicarus.common.info.CommonKV;
 import kidridicarus.common.info.UInfo;
 import kidridicarus.common.tool.Direction4;
@@ -18,28 +19,28 @@ import kidridicarus.common.tool.Direction4;
  * and if the view should be offset.
  */
 public class RoomBox extends Agent implements DisposableAgent {
-	private RoomBoxBody rbody;
+	private RoomBoxBody body;
 	private enum RoomType { CENTER, HSCROLL, VSCROLL }
-	private RoomType roomtype;
-	private String roommusic;
+	private RoomType roomType;
+	private String roomMusicStr;
 	private float viewVerticalOffset;
 	private Direction4 viewScrollDir;
 
 	public RoomBox(Agency agency, ObjectProperties properties) {
 		super(agency, properties);
 
-		rbody = new RoomBoxBody(this, agency.getWorld(), Agent.getStartBounds(properties));
+		body = new RoomBoxBody(this, agency.getWorld(), Agent.getStartBounds(properties));
 
-		roomtype = RoomType.CENTER;
+		roomType = RoomType.CENTER;
 		String roomTypeStr = properties.get(CommonKV.Room.KEY_ROOMTYPE, "", String.class);
 		if(roomTypeStr.equals(CommonKV.Room.VAL_ROOMTYPE_HSCROLL))
-			roomtype = RoomType.HSCROLL;
+			roomType = RoomType.HSCROLL;
 		else if(roomTypeStr.equals(CommonKV.Room.VAL_ROOMTYPE_VSCROLL))
-			roomtype = RoomType.VSCROLL;
+			roomType = RoomType.VSCROLL;
 		else if(roomTypeStr.equals(CommonKV.Room.VAL_ROOMTYPE_CENTER))
-			roomtype = RoomType.CENTER;
-		roommusic = properties.get(CommonKV.Room.KEY_ROOMMUSIC, "", String.class);
-		agency.getEar().registerMusic(roommusic);
+			roomType = RoomType.CENTER;
+		roomMusicStr = properties.get(CommonKV.Room.KEY_ROOMMUSIC, "", String.class);
+		agency.getEar().registerMusic(roomMusicStr);
 
 		viewVerticalOffset = UInfo.P2M(properties.get(CommonKV.Room.KEY_VIEWOFFSET_Y, 0f, Float.class));
 		viewScrollDir = Direction4.fromString(properties.get(CommonKV.Room.KEY_ROOM_SCROLL_DIR, "", String.class));
@@ -52,57 +53,85 @@ public class RoomBox extends Agent implements DisposableAgent {
 		else
 			prevCenter = incomingPrevCenter;
 
-		Vector2 center = new Vector2();
-		switch(roomtype) {
+		Vector2 center;
+		switch(roomType) {
 			case HSCROLL:
-				// if allowed only scroll right then allow only increase x position
-				if(viewScrollDir == Direction4.RIGHT)
-					center.x = playerPosition.x > prevCenter.x ? playerPosition.x : prevCenter.x;
-				// if allowed only scroll left then allow only decrease x position
-				else if(viewScrollDir == Direction4.LEFT)
-					center.x = playerPosition.x < prevCenter.x ? playerPosition.x : prevCenter.x;
-				else
-					center.x = playerPosition.x;
-
-				center.y = rbody.getBounds().y + rbody.getBounds().height/2f + viewVerticalOffset;
+				center = getHscrollCenter(playerPosition, prevCenter);
 				break;
 			case VSCROLL:
-				center.x = rbody.getBounds().x + rbody.getBounds().width/2f;
-
-				// if allowed only scroll up then allow only increase y position
-				if(viewScrollDir == Direction4.UP)
-					center.y = playerPosition.y > prevCenter.y ? playerPosition.y : prevCenter.y;
-				// if allowed only scroll left then allow only decrease x position
-				else if(viewScrollDir == Direction4.DOWN)
-					center.y = playerPosition.y < prevCenter.y ? playerPosition.y : prevCenter.y;
-				else
-					center.y = playerPosition.y;
+				center = getVscrollCenter(playerPosition, prevCenter);
 				break;
 			case CENTER:
 			default:
-				center.x = rbody.getBounds().x + rbody.getBounds().width/2f;
-				center.y = rbody.getBounds().y + rbody.getBounds().height/2f + viewVerticalOffset;
+				center = getCenterCenter();
 				break;
 		}
 		return center;
 	}
 
+	private Vector2 getHscrollCenter(Vector2 playerPosition, Vector2 prevCenter) {
+		Vector2 nextCenter = playerPosition.cpy();
+		// if allowed only scroll right then allow only increase x position
+		if(viewScrollDir == Direction4.RIGHT)
+			nextCenter.x = playerPosition.x > prevCenter.x ? playerPosition.x : prevCenter.x;
+		// if allowed only scroll left then allow only decrease x position
+		else if(viewScrollDir == Direction4.LEFT)
+			nextCenter.x = playerPosition.x < prevCenter.x ? playerPosition.x : prevCenter.x;
+		else
+			nextCenter.x = playerPosition.x;
+
+		// minX = left bound of room + half screen width
+		float minX = body.getBounds().x + UInfo.P2M(CommonInfo.V_WIDTH)/2f;
+		if(nextCenter.x < minX)
+			nextCenter.x = minX;
+		// maxX = right bound of room - half screen width
+		float maxX = body.getBounds().x + body.getBounds().width - UInfo.P2M(CommonInfo.V_WIDTH)/2f;
+		if(nextCenter.x > maxX)
+			nextCenter.x = maxX;
+
+		nextCenter.y = body.getBounds().y + body.getBounds().height/2f + viewVerticalOffset;
+
+		return nextCenter;
+	}
+
+	private Vector2 getVscrollCenter(Vector2 playerPosition, Vector2 prevCenter) {
+		Vector2 nextCenter = playerPosition.cpy();
+
+		nextCenter.x = body.getBounds().x + body.getBounds().width/2f;
+
+		// if allowed only scroll up then allow only increase y position
+		if(viewScrollDir == Direction4.UP)
+			nextCenter.y = playerPosition.y > prevCenter.y ? playerPosition.y : prevCenter.y;
+		// if allowed only scroll left then allow only decrease x position
+		else if(viewScrollDir == Direction4.DOWN)
+			nextCenter.y = playerPosition.y < prevCenter.y ? playerPosition.y : prevCenter.y;
+		else
+			nextCenter.y = playerPosition.y;
+
+		return nextCenter;
+	}
+
+	private Vector2 getCenterCenter() {
+		return new Vector2(body.getBounds().x + body.getBounds().width/2f,
+				body.getBounds().y + body.getBounds().height/2f + viewVerticalOffset);
+	}
+
 	public String getRoommusic() {
-		return roommusic;
+		return roomMusicStr;
 	}
 
 	@Override
 	public Vector2 getPosition() {
-		return rbody.getPosition();
+		return body.getPosition();
 	}
 
 	@Override
 	public Rectangle getBounds() {
-		return rbody.getBounds();
+		return body.getBounds();
 	}
 
 	@Override
 	public void disposeAgent() {
-		rbody.dispose();
+		body.dispose();
 	}
 }
